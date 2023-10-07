dc57e072bf68fa2e162efc8a4a3399d9afcd2986
Ngosch <carl1312@gmail.com>
2023-10-07 14:21:34 +0900

    comp

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-# GETメソッド、POSTメソッドの追記
-@app.route("/", methods=["GET", "POST"])
-def index():
-    # もしGETリクエストだった場合はindex.htmlを表示
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        # もしPOSTリクエストだった場合はindex.htmlでmessage変数を使う
-        message = request.form['message']
-        return render_template("index.html", message=message)
+# GETの時のルーティング
+@app.route("/", methods=["GET"])
+# @app.get("/")  # これでもOK
+def index_get():
+    return render_template("index.html")
+
+
+# POSTの時のルーティング
+@app.route("/", methods=["POST"])
+# @app.post("/")  # これでもOK
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
